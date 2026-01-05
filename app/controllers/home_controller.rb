653ae2347e887d1998c9b962c8d133f8fd0fe360
Ines de Matos <itm.m92@gmail.com>
2026-01-05 13:52:44 +0000

    Setup basic markers in maps (#36)
    - cleanup some code
    - added RGEO missing gems 
    - renamed leftovers of markup to marker
    - showing markers in map from database
    - added seeds for the markers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   def index
+    @markers = Marker.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,44 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+factory = RGeo::Geographic.spherical_factory(srid: 4326)
+
+points = [
+  {
+    name: "Point 1",
+    description: "something about point 1",
+    coordinates: factory.point(-9.1390, 38.7169)
+  },
+  {
+    name: "Point 2",
+    description: "something about point 2",
+    coordinates: factory.point(-9.1605, 38.7302)
+  },
+  {
+    name: "Point 3",
+    description: "something about point 3",
+    coordinates: factory.point(-9.1267, 38.7075)
+  },
+  {
+    name: "Point 4",
+    description: "something about point 4",
+    coordinates: factory.point(-9.1500, 38.7223)
+  },
+  {
+    name: "Point 5",
+    description: "something about point 5",
+    coordinates: factory.point(-9.1458, 38.7401)
+  }
+]
+
+points.each do |point|
+  Marker.create!(
+    name: point[:name],
+    description: point[:description],
+    longitude: point[:coordinates].longitude,
+    latitude: point[:coordinates].latitude,
+    coordinates: point[:coordinates],
+  )
+end
